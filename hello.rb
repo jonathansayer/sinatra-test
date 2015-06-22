5ea0939f951391632b8259a52316dc70db46bde6
Makers Laptop 88 <makerslaptop88@Makerss-MacBook-Pro.local>
2015-06-22 12:33:33 +0100

    Added a picture with a border to our web page

--- hello.rb
+++ hello.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
 get '/' do
-	'hello, Lucy and Jon!'
+	"<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
 
-get'/secret' do 
+get '/secret' do 
 'This is a not so secret page'
 end
